d0414c48a4405d470ef28e37642a399f56fdf1f3
owaisbaloch00 <balochowais242@gmail.com>
2025-12-01 18:32:13 +0500

    file added

---
+++ assigment-15/script.js
@@ -0,0 +1,22 @@
+ 
+//  question no 1
+function a(){
+    alert("hello world")
+}
+
+//  question no 2
+
+function b(){
+    alert("hello world")
+}
+
+
+
+function c(){
+    alert("hello world")
+}
+
+//  question no 3
+function deleteRow(btn) {
+    btn.parentElement.parentElement.remove();
+  }
---
+++ form/script.js
@@ -0,0 +1,7 @@
+let val1 = document.getElementById('email');
+let val2 = document.getElementById('pass');
+
+function btn() {
+    console.log(val1.value);
+    console.log(val2.value);
+}   
